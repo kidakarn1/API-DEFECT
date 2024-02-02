c8e471409983bb9c71490b5664479005bf40e7a9
TBKT-SYSTEM <117958477+TBKT-SYSTEM@users.noreply.github.com>
2024-02-02 12:07:09 +0700

    API DEFECT SERVER .77
    API DEFECT SERVER .77

---
+++ application/controllers/getDataMaster.php
@@ -0,0 +1,12 @@
+<?php
+class MasterData extends CI_Controller
+{
+    public function __construct() {
+
+        $this->fa = $this->load->database('tbkkfa01_db');
+    }
+
+    public function getDepartmentMaster() {
+        
+    }
+}
